c1653b1970f8610648415565f01eefca9044664a
Eduardo <du_du.mba@hotmail.com>
2020-09-24 14:47:25 +0100

    frontend finalized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -110,7 +110,7 @@ const Dashboard: React.FC = () => {
                     {transaction.type === 'outcome' && '- '}
                     {transaction.formattedValue}
                   </td>
-                  <td>{transaction.category.title}</td>
+                  <td>{transaction.title}</td>
                   <td>{transaction.formattedDate}</td>
                 </tr>
               ))}
